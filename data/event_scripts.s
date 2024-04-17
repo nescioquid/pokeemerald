d0273763253d3578125bef649a2a26e7193893d2
nescioquid <info@creachr.com>
2024-04-16 21:54:39 -0600

    removes mention of Ralts as Wally's Pokémon and replaces it with an Eevee

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1077,3 +1077,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/gt/overwold_mons.inc"
 .include "data/scripts/pory/gt/npc_dialogues.inc"
 .include "data/scripts/pory/gt/trade.inc"
+.include "data/scripts/pory/gt/wally.inc"
