c32761a3048d78456f2d5153e5696a72f80764fb
nescioquid <info@creachr.com>
2024-04-16 23:14:58 -0600

    changes Game Corner prizes

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1083,3 +1083,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/gym_guide.inc"
 .include "data/scripts/pory/items/diving_treasure_hunter.inc"
 .include "data/scripts/pory/items/fossil_maniac.inc"
+.include "data/scripts/pory/items/game_corner.inc"
