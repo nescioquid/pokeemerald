945fec51c0de6dea450b84d11144cb1d6105b56f
nescioquid <info@creachr.com>
2024-04-18 14:21:40 -0600

    changes Steven to give an Old Amber instead of HM08 Dive

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1079,3 +1079,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/winstrates.inc"
 .include "data/scripts/pory/items/devon_employee.inc"
 .include "data/scripts/pory/items/mr_stone.inc"
+.include "data/scripts/pory/items/steven_mossdeep.inc"
