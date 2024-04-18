177919dc52ea73336fdf338390724636809d8ad8
nescioquid <info@creachr.com>
2024-04-18 16:08:58 -0600

    changes and adds NPCs that give ORAS TMs

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1095,3 +1095,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/fossil_maniac.inc"
 .include "data/scripts/pory/items/game_corner.inc"
 .include "data/scripts/pory/items/gym_leaders.inc"
+.include "data/scripts/pory/items/tms_hms.inc"
