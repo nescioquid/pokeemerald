0214139819b62e879b20c234b602856c64005286
nescioquid <info@creachr.com>
2024-04-18 15:47:19 -0600

    adds a maniac in Slateport who gives evo stones in exchange for fossils

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1082,3 +1082,4 @@ EventScript_VsSeekerChargingDone::
 .include "data/scripts/pory/items/steven_mossdeep.inc"
 .include "data/scripts/pory/items/gym_guide.inc"
 .include "data/scripts/pory/items/diving_treasure_hunter.inc"
+.include "data/scripts/pory/items/fossil_maniac.inc"
